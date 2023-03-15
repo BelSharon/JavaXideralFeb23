a5a8d515ed5a051e4036ac53b9efcd22943e95b4
SBTR <03sh.bel.tr@gmail.com>
2023-03-14 23:14:13 -0600

    Implementación de backend de CRUD que consume el proyecto springBootRestJpa

---
+++ clientRest/src/main/java/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,94 @@
+package com.luv2code.springdemo.service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	private RestTemplate restTemplate;
+	private String crmRestUrl;
+	private Logger logger = Logger.getLogger(getClass().getName());
+
+	@Autowired
+	public CustomerServiceImpl(RestTemplate theRestTemplate,
+			@Value("${crm.rest.url}") String theUrl) {
+		restTemplate = theRestTemplate;
+		crmRestUrl = theUrl;
+		logger.info("Loaded property: crm.rest.url="+ crmRestUrl);
+	}
+
+	@Override
+	public List<Customer> getCustomers() {
+
+		logger.info("in getCustomers(): Calling REST API "+ crmRestUrl);
+
+		// make REST call
+
+		ResponseEntity<List<Customer>> responseEntity =
+				restTemplate.exchange(crmRestUrl, HttpMethod.GET, null,
+						new ParameterizedTypeReference<List<Customer>>() {});
+
+		// get the list of customers from response
+
+		List<Customer> customers = responseEntity.getBody();
+
+		logger.info("in getCustomers(): customers" + customers);
+
+		return customers;	
+	
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		logger.info("in saveCustomer(): Calling REST API " + crmRestUrl);
+
+		int employeeId = theCustomer.getId();
+
+		if (employeeId == 0) {//si no hay id
+			// hace un post
+			restTemplate.postForEntity(crmRestUrl, theCustomer, String.class);
+		} else { // si hay id
+			// hace actualización
+			restTemplate.put(crmRestUrl, theCustomer);
+		}
+
+		logger.info("in saveCustomer(): success");
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+
+		logger.info("in getCustomer(): Calling REST API " + crmRestUrl);
+
+		//va por el cliente con el id
+		Customer theCustomer = restTemplate.getForObject(crmRestUrl + "/" + theId, Customer.class);
+
+		logger.info("in saveCustomer(): theCustomer=" + theCustomer);
+
+		//regresamos el cliente
+		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+
+		logger.info("in deleteCustomer(): Calling REST API " + crmRestUrl);
+
+		restTemplate.delete(crmRestUrl + "/" + theId);
+
+		logger.info("in deleteCustomer(): deleted customer theId="+ theId);
+	}
+	
+}
